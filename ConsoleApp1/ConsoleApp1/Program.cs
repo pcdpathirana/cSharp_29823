c91fb0a8716494b06e4f5ce4631e0eb6c5a4c176
Chithma Dilmin <Chithma Dilmin@DESKTOP-97LOFU2>
2024-01-31 13:44:50 +0530

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length:- ");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter width:- ");
+            int width = int.Parse(Console.ReadLine());
+
+            int area = area(length,width);
+
+            Console.WriteLine("Area is "+area);
+
+            static int(int.parse)
+            
+
+        }
+    }
+}
