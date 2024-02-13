105566f69782181c0976cf76cd6d93caa6a17d83
Chithma Dilmin <pcdpathirana@students.nsbm.ac.lk>
2024-02-13 19:40:21 +0530

    1st sttempt

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter length:- ");
-            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter student's name: ");
+            string name = Console.ReadLine();
 
-            Console.Write("Enter width:- ");
-            int width = int.Parse(Console.ReadLine());
+            Console.Write("Enter exam marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
 
-            int area = area(length,width);
+            if (marks < 0 || marks > 100)
+            {
+                Console.WriteLine("Error: Marks should be between 0 and 100.");
+            }
+            else
+            {
+                string grade;
 
-            Console.WriteLine("Area is "+area);
+                if (marks >= 75 && marks <= 100)
+                {
+                    grade = "A";
+                }
+                else if (marks >= 60 && marks <= 74)
+                {
+                    grade = "B";
+                }
+                else if (marks >= 50 && marks <= 59)
+                {
+                    grade = "C";
+                }
+                else if (marks >= 40 && marks <= 49)
+                {
+                    grade = "D";
+                }
+                else
+                {
+                    grade = "Fail";
+                }
 
-            static int(int.parse)
-            
-
+                Console.WriteLine($"Student Name: {name}");
+                Console.WriteLine($"Grade: {grade}");
+            }
         }
     }
 }
